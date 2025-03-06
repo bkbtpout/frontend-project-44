034f08ee61241a5d24b1bfdbcc2168f52062316b
paveldelei3020 <paveldelei200@gmail.com>
2025-03-06 11:33:27 +0300

    step three

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 const readlineSync = require('readline-sync');
 
 module.exports = () => {
-  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
